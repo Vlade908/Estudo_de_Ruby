7bcfe53fcb94bd79afb4afb37d88d575c8d43d98
Enzo Alves <enzoalves.trabalho@gmail.com>
2024-12-06 11:20:55 -0300

    .

--- agenda.rb
+++ agenda.rb
@@ -6,36 +6,45 @@
 def todos_contatos
     @agenda.each do |contato|
             puts "#{contato[:nome]} - #{contato[:telefone]}"
+            puts "--------------------"
     end
 end
 
+def adicionar_contatos
+    print "Nome: "
+    nome = gets.chomp
+    print "Telefone: "
+    telefone = gets.chomp
+
+    @agenda << {nome: nome, telefone: telefone}
+end
+
 loop do
-  puts "----------------------------------"
+    puts "----------------------------------"
     puts "1.Contatos\n2. Adicionar Contatos\n3. VerContato\n4. Editar Contato\n5. Remover Contato \n0. Sair"
-  puts "----------------------------------"
+    puts "----------------------------------"
     codigo = gets.chomp.to_i
     case
     when codigo == 0
       puts "Até mais!"
       break
     when codigo == 1
-      puts "------Contatos ----------"
+      puts "====== Contatos ======"
       todos_contatos
-      puts "----------------------"
-
-      break
+      puts "======================"
     when codigo == 2
+      adicionar_contatos
       
-      break
+      
     when codigo == 3
       
-      break
+      
     when codigo == 4
       
-      break
+      
     when codigo == 5
       
-      break
+      
     end
     
 end
